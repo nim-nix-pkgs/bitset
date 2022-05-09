cd6fc01eda873f3ebb14a51f3fa7c65378c5cead
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:38 -0300

    chore: re index bitset

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."bitset-main".dir   = "main";
-  inputs."bitset-main".owner = "nim-nix-pkgs";
-  inputs."bitset-main".ref   = "master";
-  inputs."bitset-main".repo  = "bitset";
-  inputs."bitset-main".type  = "github";
-  inputs."bitset-main".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."bitset-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
